6db9592cc763cc5df38f0c56dba1ab7b999c765e
Thiago Rodrigues <hey@thiagorodrigues.dev>
2022-05-15 18:52:02 -0300

    chore: create str-producer project

---
+++ str-producer/src/main/kotlin/dev/trodrigues/strproducer/StrProducerApplication.kt
@@ -0,0 +1,11 @@
+package dev.trodrigues.strproducer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StrProducerApplication
+
+fun main(args: Array<String>) {
+    runApplication<StrProducerApplication>(*args)
+}
---
+++ str-producer/src/test/kotlin/dev/trodrigues/strproducer/StrProducerApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.trodrigues.strproducer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StrProducerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
